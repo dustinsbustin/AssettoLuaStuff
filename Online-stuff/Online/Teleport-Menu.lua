cadcd044c26549fe55c8fc9db0181478f6df3005
dustinsbustin <dustin@dustin.gg>
2023-06-07 19:57:11 -0500

    fix

--- Online-stuff/Online/Teleport-Menu.lua
+++ Online-stuff/Online/Teleport-Menu.lua
@@ -53,9 +53,11 @@ function CartoCar_UI()
 				if ui.button("Teleport") and selectedCar and timer.running <= 0 then -- check if car selected/button pressed/timer above 0
 					timer.running = timer.length
 					local dir = selectedCar.look
-					-- physics.setCarVelocity(0, vec3(0, 0, 0)) --reset velocity
-					-- spawn 8 meters behind, add 0.1 meter height to avoid falling through the map
-					physics.setCarPosition(0, selectedCar.position + vec3(0, 0.1, 0) - dir * 8, -dir)
+
+                    local teleportee = physics.getCarVelocity(selectedCar)
+					physics.setCarVelocity(0, teleportee) -- mirror velocity
+					
+					physics.setCarPosition(0, selectedCar.position + vec3(0, 0.1, 0) - dir * 10, -dir) -- spawn 8 meters behind, add 0.1 meter height to avoid falling through the map
 				end
 			end
 		end
